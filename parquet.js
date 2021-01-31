98efa10d1c75389a81c558d7957c17bdb32497a5
Dustin Charles <dustin@entitycs.com>
2021-01-31 16:45:34 +0000

    Hack BigInt.prototype.toJSON as per https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt

--- parquet.js
+++ parquet.js
@@ -4,6 +4,10 @@ const schema = require('./lib/schema');
 const shredder = require('./lib/shred');
 const util = require('./lib/util');
 
+// per https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+//eslint-disable-next-line no-extend-native, max-statements-per-line
+BigInt.prototype.toJSON = function () { return this.toString(); };
+
 module.exports = {
   ParquetEnvelopeReader: reader.ParquetEnvelopeReader,
   ParquetReader: reader.ParquetReader,
