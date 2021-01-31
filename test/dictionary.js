8b1d47d7216213287ea50d226cb5a5739b53923e
Dustin Charles <dustin@entitycs.com>
2021-01-31 16:57:57 +0000

    Pull changes from testFix/bigInt, and fix indentation.

--- test/dictionary.js
+++ test/dictionary.js
@@ -5,8 +5,8 @@ const assert = chai.assert;
 const parquet = require('../parquet.js');
 const path = require('path');
 
-describe('dictionary encoding', async function() {
-  it('should read uncompressed dictionary from spark', async function() {
+describe('dictionary encoding', async function () {
+  it('should read uncompressed dictionary from spark', async function () {
     let reader = await parquet.ParquetReader.openFile(path.resolve(__dirname, 'test-files/spark-uncompressed-dict.parquet'));
     let cursor = reader.getCursor();
     let records = [];
